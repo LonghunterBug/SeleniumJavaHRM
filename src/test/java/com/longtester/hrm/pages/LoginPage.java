17a38df4d970a79f75c325ca9cbbb33bd0272c63
LonghunterBug <tranminhlong2707@gmail.com>
2025-08-26 21:25:32 +0700

    update xpath on LoginPage

--- src/test/java/com/longtester/hrm/pages/LoginPage.java
+++ src/test/java/com/longtester/hrm/pages/LoginPage.java
@@ -7,8 +7,8 @@ import org.testng.Assert;
 
 public class LoginPage {
     private WebDriver driver;
-    private By inputUserName = By.xpath("//input[@placeholder='Username']");
-    private By inputPassword = By.xpath("//input[@placeholder='Password']");
+    private By inputUserName = By.xpath("//input[@placeholder='username']");
+    private By inputPassword = By.xpath("//input[@placeholder='password']");
     private By buttonLogin = By.xpath("//button[normalize-space()='Login']");
     private By alertErrorInvalidCredential = By.xpath("//p[normalize-space()='Invalid credentials']");
     private By alertErrorInputRequire = By.xpath("//span[contains(@class,'input-field-error-message')]");
